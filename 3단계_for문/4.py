6a16d204161884c60c1b577ca443f46428a64c16
gabang2 <offbeat1020@naver.com>
2021-10-03 20:20:51 +0900

    10.3

--- 3단계_for문/4.py
+++ 3단계_for문/4.py
@@ -1,3 +1,13 @@
-print("hello world")
-print("hello world2")
-print("hello world3")
+#입출력방식이 느리면 여러 줄을 입력받거나 출력할 때 시간초과가 날 수
+#있으므로input대신에 sys.stdin.readline을 사용하는 것이 좋다.
+
+import sys
+
+n=int(input())
+c=[]
+for i in range(n):
+    a, b=(sys.stdin.readline().rstrip()).split()
+    c.append(int(a)+int(b))
+
+for i in range(len(c)):
+    print(c[i])
