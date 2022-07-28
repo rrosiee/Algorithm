704270f986c05e373c59f4ce72ee5e528f9e343a
gabang2 <offbeat1020@naver.com>
2022-07-28 19:36:44 +0900

    개임 개발 - 구현(118p)

--- 이것이 취업을 위한 코딩테스트다 with 파이썬/[118p]게임개발-구현.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬/[118p]게임개발-구현.py
@@ -1,38 +1,33 @@
 # 이코테 118p - 풀이 과정 : ()
 
+#### 입력받기 ####
 N, M = map(int, input().split())
-temp_character = list(map(int, input().split()))
-character = dict()
-character['location'], character['compass'] = [temp_character[0], temp_character[1]], temp_character[2]
-compass = {0: {'direction': [0, 1], 'next': 3, 'back': [0, -1]}, 1: {'direction': [1, 0], 'next': 0, 'back': [-1, 0]},
-           2: {'direction': [0, -1], 'next': 3, 'back': [0, 1]}, 3: {'direction': [-1, 0], 'next': 0, 'back': [1, 0]}}
+character = list(map(int, input().split()))
 land = list()
+compass = {0: [-1, 0], 1: [0, 1], 2: [1, 0], 3: [0, -1]}
 result = 1
-
 for i in range(N):
     land.append(list(map(int, input().split())))
 
+#### 계산하기 ####
 while True:
-    character['compass'] = compass[character['compass']]['next']
-    print(character['location'][0] + compass[character['compass']]['direction'][0])
-    if 0 <= character['location'][0] + compass[character['compass']]['direction'][0] < M \
-            and 0 <= character['location'][1] + compass[character['compass']]['direction'][1] < N \
-            and land[character['location'][0] + compass[character['compass']]['direction'][0]][
-        character['location'][1] + compass[character['compass']]['direction'][1]] == 0:
-        print('zlkdf')
-        character['location'][0] += compass[character['compass']]['direction'][0]
-        character['location'][1] += compass[character['compass']]['direction'][1]
-        result += 1
+    # 캐릭터를 왼쪽으로 회전
+    character[2] = (character[2] + 3) % 4
+    land[character[0]][character[1]] = 1
+    front_character = [character[0] + compass[character[2]][0], character[1] + compass[character[2]][1]]
+    back_character = [character[0] + (compass[character[2]][0] * -1), character[1] + (compass[character[2]][1] * -1)]
+
+    if 0 <= front_character[0] < M and 0 <= front_character[1] < N \
+            and land[front_character[0]][front_character[1]] == 0:
+        character[0], character[1] = front_character[0], front_character[1]
+
+    elif 0 <= back_character[0] < M and 0 <= back_character[1] < N \
+            and land[back_character[0]][back_character[1]] == 0:
+        character[0], character[1] = back_character[0], back_character[1]
+
     else:
-        print('dsfls')
-        if 0 <= character['location'][0] + compass[character['compass']]['back'][0] < M \
-                and 0 <= character['location'][1] + compass[character['compass']]['back'][1] < N \
-                and land[character['location'][0] + compass[character['compass']]['back'][0]] \
-                [character['location'][1] + compass[character['compass']]['back'][1]] == 0:
-            character['location'][0] += compass[character['compass']]['back'][0]
-            character['location'][1] += compass[character['compass']]['back'][1]
-            result += 1
-        else:
-            break
+        break
+
+    result += 1
 
 print(result)
