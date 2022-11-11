ceca786260236da011a619853e308bb66a3c45b0
gabang2 <offbeat1020@naver.com>
2022-11-11 16:56:46 +0900

    퀵 정렬 ver1 & ver2 구현

--- 이것이 취업을 위한 코딩테스트다 with 파이썬/[156p]정렬 알고리즘.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬/[156p]정렬 알고리즘.py
@@ -11,6 +11,7 @@ def selection_sort(array):
                 array[i], array[j] = array[j], array[i]
     print(array)
 
+## 삽입 정렬 ##
 def insertion_sort(array):
     for i in range(1, len(array)):
         for k in range(i, 0, -1):
@@ -20,6 +21,43 @@ def insertion_sort(array):
                 break
     print(array)
 
-insertion_sort([10, 9, 4])
-selection_sort([10, 9, 8, 7]) # 선택 정렬
-print(sorted([10, 9, 8, 7])) # 파이썬 내장 정렬 함수
+## 퀵 정렬 ##
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left = start + 1
+    right = end
+    while left <= right:
+        while left <= end and array[left] <= array[pivot]:
+            left += 1
+        while right > start and array[right] >= array[pivot]:
+            right -= 1
+        if left > right:
+            array[right], array[pivot] = array[pivot], array[right]
+        else:
+            array[left], array[right] = array[right], array[left]
+
+    quick_sort(array, start, right-1) # 왼쪽 부분
+    quick_sort(array, right+1, end) # 오른쪽 부분
+    return array
+
+## 파이썬의 장점을 살린 퀵 정렬 코드
+def quick_sort_ver2(array):
+    if len(array) <= 1:
+        return array
+    pivot = array[0]
+    tail = array[1:]
+    
+    left_side = [x for x in tail if x <= pivot]
+    right_side = [x for x in tail if x > pivot]
+
+    return quick_sort_ver2(left_side) + [pivot] + quick_sort_ver2(right_side)
+
+
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+print(quick_sort(array, 0, len(array)-1))
+print(quick_sort_ver2(array))
+insertion_sort(array)
+selection_sort(array) # 선택 정렬
+print(sorted(array)) # 파이썬 내장 정렬 함수
