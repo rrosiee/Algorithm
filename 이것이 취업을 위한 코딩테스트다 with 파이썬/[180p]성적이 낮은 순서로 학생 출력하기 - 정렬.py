dd441ef4c7ec79d1f816f728e98326471befbdd2
gabang2 <offbeat1020@naver.com>
2022-11-11 18:33:05 +0900

    성적이 낮은 순서로 학생 출력하기 - 정렬(180p)==수정 후

--- 이것이 취업을 위한 코딩테스트다 with 파이썬/[180p]성적이 낮은 순서로 학생 출력하기 - 정렬.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬/[180p]성적이 낮은 순서로 학생 출력하기 - 정렬.py
@@ -6,9 +6,6 @@ for i in range(N):
     temp = input().split()
     array.append((temp[0], int(temp[1])))
 
-def select(array):
-    return array[1]
-
-array.sort(key=select)
+array.sort(key=lambda array: array[1])
 for i in array:
     print(i[0], end=' ')
