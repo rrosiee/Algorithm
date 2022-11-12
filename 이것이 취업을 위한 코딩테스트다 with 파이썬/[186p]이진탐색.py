abc5e9c4eb2be8518576b988c6b35e3a3fdd0c9a
gabang2 <offbeat1020@naver.com>
2022-11-12 19:30:21 +0900

    이진 탐색에서 입력 받는 방법(sys.stdin.readline().rstrip())

--- 이것이 취업을 위한 코딩테스트다 with 파이썬/[186p]이진탐색.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬/[186p]이진탐색.py
@@ -1,7 +1,7 @@
-# 이코테 186쪽
+'''# 이코테 186쪽 - (https://velog.io/@gabang2/%EC%9D%B4%EC%A7%84-%ED%83%90%EC%83%89)
 
 ## 순차 탐색 ##
-'''def sequential_search(n, target, array):
+def sequential_search(n, target, array):
     for i in range(n):
         if array[i] == target:
             return i+1
@@ -31,8 +31,10 @@ result = binary_search(array, target, 0, len(array))
 if result == None:
     print('원소가 존재하지 않습니다.')
 else:
-    print(result + 1)'''
+    print(result + 1)
 
+
+## 이진 탐색 ver 2 ##
 def binary_search2(array, target, start, end):
     while start <= end:
         mid = (start + end) // 2
@@ -50,4 +52,10 @@ result = binary_search2(array, target, 0, n-1)
 if result == None:
     print('원소가 존재하지 않습니다.')
 else:
-    print(result + 1)
+    print(result + 1)'''
+
+## 이진 탐색에서 빠르게 입력받는 방법 ##
+'''input()함수를 이용하면 동작 속도가 느려져 시간 초과로 오답 판정을 받을 수 있음'''
+import sys
+input_data = sys.stdin.readline().rstrip()
+print(input_data)
