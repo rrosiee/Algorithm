286a27ae4b080029ef819fa9bf0a5ed8ebfb663c
gabang2 <offbeat1020@naver.com>
2022-11-26 20:57:12 +0900

    미래도시 - 최단경로 알고리즘(259쪽)

--- 이것이 취업을 위한 코딩테스트다 with 파이썬/[259p] 미래도시 - 최단 경로 알고리즘.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬/[259p] 미래도시 - 최단 경로 알고리즘.py
@@ -1,2 +1,30 @@
 # 이코테 259쪽
 
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+# 입력받음
+N, M = map(int, input().split())
+graph = [[INF]*(N+1) for _ in range(N+1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+X, K = map(int, input().split())
+
+for i in range(1, N+1):
+    graph[i][i] = 0
+
+for k in range(1, N+1):
+    for a in range(1, N+1):
+        for b in range(1, N+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+for i in graph:
+    print(i)
+
+if graph[1][K] == INF or graph[K][X] == INF:
+    print(-1)
+else:
+    print(graph[1][K] + graph[K][X])
