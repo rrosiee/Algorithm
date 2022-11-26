3687d2e692b38764e9d771b05b9d7fe9aae947d7
gabang2 <offbeat1020@naver.com>
2022-11-26 20:18:17 +0900

    플로이드 워셜 알고리즘 복습하기

--- test.py
+++ test.py
@@ -1,30 +1,22 @@
 import sys
-import heapq
 input = sys.stdin.readline
 INF = int(1e9)
 
-n, m = map(int, input().split())
-start = int(input())
-graph = [[] for _ in range(n+1)]
-distance = [INF] * (n+1)
+n = int(input())
+m = int(input())
+graph = [[INF] * (n+1) for _ in range(n+1)]
 
 for _ in range(m):
     a, b, c = map(int, input().split())
-    graph[a].append((b, c))
+    graph[a][b] = c
 
-def dijkstra(start):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
-        for i in graph[now]:
-            cost = distance[now] + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+for i in range(1, n+1):
+    graph[i][i] = 0
 
-dijkstra(start)
-print(distance)
+for i in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][i] + graph[i][b])
+
+for i in graph:
+    print(i)
