741290cbb74fd5a40cfb354483e018ae632867f6
gabang2 <offbeat1020@naver.com>
2023-03-24 23:05:59 +0100

    그래프 이론 간단 ver 복습하기

--- 이것이 취업을 위한 코딩테스트다 with 파이썬/[273p] 그래프 이론.py
+++ 이것이 취업을 위한 코딩테스트다 with 파이썬/[273p] 그래프 이론.py
@@ -1,4 +1,4 @@
-# 이코테 273쪽
+# 이코테 273쪽 - 서로소 집합 자료구조
 
 def find_parent(parent, x):
     if parent[x] != x:
@@ -8,27 +8,34 @@ def find_parent(parent, x):
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
-    if a<b:
+    
+    if a < b:
         parent[b] = a
     else:
         parent[a] = b
-    
+
+# 값 초기화 하기
+'''
+v : 노드의 수
+e : 합치기 연산 (Union)의 수
+'''
 v, e = map(int, input().split())
-parent = [0] * (v + 1)
+parent = [0] * (v+1)
 
 for i in range(1, v+1):
     parent[i] = i
 
-for i in range(e):
+# 합치기 연산 수행
+for _ in range(e):
     a, b = map(int, input().split())
     union_parent(parent, a, b)
 
-print('각 원소가 속한 집합 : ', end='')
+# 출력 - 집합 출력하기
+print("집합 : ", end='')
 for i in range(1, v+1):
     print(find_parent(parent, i), end=' ')
 
-print()
-
-print('부모테이블: ', end='')
+# 출력 - 부모 테이블 출력하기
+print("부모 테이블 : ", end='')
 for i in range(1, v+1):
     print(parent[i], end=' ')
