020ed343ba52b9acc60a0bbe9a9580533b88f163
gabang2 <offbeat1020@naver.com>
2023-08-09 21:08:24 +0900

    미로 탈출 -책 솔루션

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
-def factorial_iterative(n):
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
-
-
-def factorial_recursive(n):
-    if n <= 1:
-        return 1
-    return n * factorial_recursive(n - 1)
+arr = [[1, 2],[3, 4]]
+print(arr[0][1])
