2f09322101d4c55ea1dc2eb6a6992e4fe3289333
gabang2 <offbeat1020@naver.com>
2023-10-19 17:09:51 +0900

    프로그래머스 60061 : 기둥과 보(시간 초과로 인해 10/30에 다시 풀어보기

--- 구현/[프로그래머스 60061] 기둥과 보 설치.py
+++ 구현/[프로그래머스 60061] 기둥과 보 설치.py
@@ -20,13 +20,25 @@ def do_or_not(frame_one, frame_current):
 
     # == frame_temp가 모든 조건을 만족하는지 확인 == #
     for frame_temp_one in frame_temp:
+        # 기둥과 보 나누기
+        bo_list = list()
+        gidung_list = list()
+        frame_temp_2 = copy.deepcopy(frame_temp)
+        frame_temp_2.remove(frame_temp_one)
+        for f in frame_temp_2:
+            if f[2] == 1:
+                bo_list.append([f[0], f[1]])
+                bo_list.append([f[0] + 1, f[1]])
+            if f[2] == 0:
+                gidung_list.append([f[0], f[1] + 1])
+
         # 기둥일 경우 (frame_temp_one 3번째 원소 : 0)
         if frame_temp_one[2] == 0:
-            if gidung_valid(frame_temp_one, frame_temp) == False:
+            if gidung_valid(frame_temp_one, bo_list, gidung_list) == False:
                 return frame_current
         # 보일 경우 (frame_temp_one 3번째 원소 : 1)
         elif frame_temp_one[2] == 1:  #
-            if bo_valid(frame_temp_one, frame_temp) == False:
+            if bo_valid(frame_temp_one, bo_list, gidung_list) == False:
                 return frame_current
 
         else:
@@ -34,18 +46,7 @@ def do_or_not(frame_one, frame_current):
     return frame_temp
 
 
-def gidung_valid(frame_temp_one, frame_temp):
-    # 0. 기둥과 보 나누기
-    bo_list = list()
-    gidung_list = list()
-    frame_temp_2 = copy.deepcopy(frame_temp)
-    frame_temp_2.remove(frame_temp_one)
-    for f in frame_temp_2:
-        if f[2] == 1:
-            bo_list.append([f[0], f[1]])
-            bo_list.append([f[0] + 1, f[1]])
-        if f[2] == 0:
-            gidung_list.append([f[0], f[1] + 1])
+def gidung_valid(frame_temp_one, bo_list, gidung_list):
 
     # 1. 기둥은 바닥 위에 있다.
     if frame_temp_one[1] == 0:
@@ -54,79 +55,24 @@ def gidung_valid(frame_temp_one, frame_temp):
     # 2. 기둥은 보의 한쪽 끝에 있다.
     if frame_temp_one[0:2] in bo_list:
         return True
-    # for bo in frame_temp:
-    #
-    #     if bo[2] == 1:
-    #
-    #         start = copy.deepcopy(bo[0:2])
-    #         right = copy.deepcopy(start)
-    #         right[0] += 1
-    #         if start == frame_temp_one[0:2] or right == frame_temp_one[0:2]:
-    #             return True
 
     # 3. 기둥은 다른 기둥의 위에 있다.
     if frame_temp_one[0:2] in gidung_list:
         return True
-    # frame_temp_2 = copy.deepcopy(frame_temp)
-    # frame_temp_2.remove(frame_temp_one)
-    # for gidung in frame_temp_2:
-    #     if gidung[2] == 0:
-    #         up = copy.deepcopy(gidung[0:2])
-    #         up[1] += 1
-    #         if up == frame_temp_one[0:2]:
-    #             return True
 
     # 기준에 충족하지 않음
     return False
 
 
-def bo_valid(frame_temp_one, frame_temp):
-    # 0. 기둥과 보 나누기
-    bo_list = list()
-    gidung_list = list()
-    frame_temp_2 = copy.deepcopy(frame_temp)
-    frame_temp_2.remove(frame_temp_one)
-    for f in frame_temp_2:
-        if f[2] == 1:
-            bo_list.append([f[0], f[1]])
-            bo_list.append([f[0] + 1, f[1]])
-        if f[2] == 0:
-            gidung_list.append([f[0], f[1] + 1])
+def bo_valid(frame_temp_one, bo_list, gidung_list):
 
     # 1. 한쪽 끝 부분이 기둥 위에 있다.
     if frame_temp_one[0:2] in gidung_list or [frame_temp_one[0] + 1, frame_temp_one[1]] in gidung_list:
         return True
-    # right_bo = copy.deepcopy(frame_temp_one[0:2])
-    # right_bo[0] += 1
-    # for gidung in frame_temp:
-    #     if gidung[2] == 0:
-    #         up = copy.deepcopy(gidung[0:2])
-    #         up[1] += 1
-    #         if up == frame_temp_one[0:2] or up == right_bo:
-    #             return True
 
     # 2. 양쪽 끝 부분이 다른 보와 동시에 연결되어있다.
-
     if frame_temp_one[0:2] in bo_list and [frame_temp_one[0] + 1, frame_temp_one[1]] in bo_list:
         return True
-    # frame_temp_2 = copy.deepcopy(frame_temp)
-    # frame_temp_2.remove(frame_temp_one)  # 여기가 문제네
-    # start = copy.deepcopy(frame_temp_one[0:2])
-    # right = copy.deepcopy(start)
-    # right[0] += 1
-    # i = 0
-    # another_list = list()
-    #
-    # for another in frame_temp_2:
-    #     if another[2] == 1:  # 보일 경우
-    #
-    #         another_start = copy.deepcopy(another[0:2])
-    #         another_right = copy.deepcopy(another_start)
-    #         another_right[0] += 1
-    #         another_list.append(another_start)
-    #         another_list.append(another_right)
-    # if start in another_list and right in another_list:
-    #     return True
     return False
 
 
