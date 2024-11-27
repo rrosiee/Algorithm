a62c70e193c111d6ace5028053c3f34b439acf00
Gayeong Kim <offbeat1020@naver.com>
2024-11-27 21:19:53 +0900

    [level 2] Title: 괄호 회전하기, Time: 111.46 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,38 +2,41 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        
+        int result = 0;        
         Stack<Character> stack = new Stack<>();
+        
         for (int i=0; i < s.length(); i++) {
-            char[] charArray = (s.substring(i) + s.substring(0, i)).toCharArray();
-            boolean isCorrect = true;
+            String rotatedString = s.substring(i) + s.substring(0, i);
+            char[] charArray = rotatedString.toCharArray();
             stack.clear();
+        
             for (char c : charArray) {
+                
+                // 여는 괄호면 stack에 넣기
                 if (c == '{' || c == '[' || c == '(') {
                     stack.push(c);
                     continue;
                 }
                 
+                // 닫는 괄호면 짝이 맞는지 확인하기
                 if (!stack.isEmpty()) {
                     if (
                         (stack.peek() == '{' && c == '}') ||
-                        (stack.peek() == '[' && c == ']') || 
-                        (stack.peek() == '(' && c == ')')) {
+                        (stack.peek() == '[' && c == ']') ||
+                        (stack.peek() == '(' && c == ')')
+                    ) {
                         stack.pop();
                         continue;
                     }
-                } else {
-                    stack.push(c);    
                 }
+                
+                // 짝이 안맞는다면 False
+                stack.push(c);
             }
-            
             if (stack.isEmpty()) {
-                answer ++;
-            } 
+                result++;
+            }
         }
-        
-        
-        return answer;
+        return result;
     }
 }
