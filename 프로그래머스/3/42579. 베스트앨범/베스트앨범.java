49ddb5e75bda8d1dc3a3f16465e7478652e49651
Gayeong Kim <offbeat1020@naver.com>
2025-01-31 15:31:47 +0900

    [level 3] Title: 베스트앨범, Time: 7.77 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -6,28 +6,30 @@ class Solution {
         
         // Variables
         HashMap<String, Integer> playMap = new HashMap<>();
-        HashMap<String, ArrayList<int[]>> genreMap = new HashMap<>();
+        HashMap<String, ArrayList<int[]>> genreMap =  new HashMap<>();
         
-        
-        // 1. Genre별 총 play수와 Genre별 index와 play수 HashMap 생성
-        for (int i=0; i < genres.length; i++) {
-            // Genre 별 총 Play 수 계산
+        // 1. genre별 총 play수와 genre별 index의 play수 HahMap 생성
+        for (int i=0; i<genres.length; i++) {
+            
+            // Genre별 총 Play수 계산
             playMap.put(genres[i], playMap.getOrDefault(genres[i], 0) + plays[i]);
             
-            // Genre 별 index와 Play수
+            // Genre별 index와 play 수
             if (!genreMap.containsKey(genres[i])) {
-                genreMap.put(genres[i], new ArrayList<>());    
+                genreMap.put(genres[i], new ArrayList<>());
             }
+            
             genreMap.get(genres[i]).add(new int[]{i, plays[i]});
         }
         
-        // 총 재생 횟수가 많은 장르 순으로 내림차순 정렬
+        // 총 재생횟수가 많은 장르 순으로 내림차순 정렬
         ArrayList<Integer> answer = new ArrayList<>();
-        Stream<Map.Entry<String, Integer>> sortedGenre = playMap.entrySet().stream().sorted((o1, o2) -> Integer.compare(o2.getValue(), o1.getValue()));
-        sortedGenre.forEach(entry -> {
+        Stream<Map.Entry<String, Integer>> sortedGenres = playMap.entrySet().stream().sorted((o1, o2) -> Integer.compare(o2.getValue(), o1.getValue()));
+        sortedGenres.forEach(entry -> {
             Stream<int[]> sortedSongs = genreMap.get(entry.getKey()).stream().sorted((o1, o2) -> Integer.compare(o2[1], o1[1])).limit(2);
             sortedSongs.forEach(song -> answer.add(song[0]));
-        });
+            }
+        );
         
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
